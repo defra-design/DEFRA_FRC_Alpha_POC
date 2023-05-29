0b470cae9e6bf7c73d1f74fed48636b4f781902a
vivek-sekhar <vivek.sekhar@hmcts.net>
2023-05-29 21:07:01 +0100

    feat(latest-screen-changes): Code refeactor and update screen designs

--- app/routes.js
+++ app/routes.js
@@ -5,7 +5,7 @@
 
 const govukPrototypeKit = require('govuk-prototype-kit')
 const router = govukPrototypeKit.requests.setupRouter()
-const CommodityVolumeGetController_Router = govukPrototypeKit.requests.setupRouter()
+/*const CommodityVolumeGetController_Router = govukPrototypeKit.requests.setupRouter()
 const AnnualIncomeController = govukPrototypeKit.requests.setupRouter()
 
 const ParentCompanyController = govukPrototypeKit.requests.setupRouter()
@@ -18,264 +18,193 @@ const Static_AmendedCommodityController = govukPrototypeKit.requests.setupRouter
 const AppComplete_AmendedCommodityController = govukPrototypeKit.requests.setupRouter()
 const ExemptionPeriodController = govukPrototypeKit.requests.setupRouter()
 const AmendedExemptionPeriodController = govukPrototypeKit.requests.setupRouter()
-const CommodityMethodsController = govukPrototypeKit.requests.setupRouter()
-const exmap = new Map();
+const CommodityMethodsController = govukPrototypeKit.requests.setupRouter()*/
 
 router.post('/Login-answer', function (req, res) {
+  const accountholder = req.session.data['Accountholder'];
 
-  var Accountholder = req.session.data['Accountholder'];
-
-
-  // Check whether the variable matches a condition
-  if (Accountholder == "Yes") {
-    // Send user to next page
+  if (accountholder == "Yes") {
     res.redirect('/signin')
   } else {
-    // Send user to ineligible page
     res.redirect('/create-account')
   }
+});
 
-},
-  ExemptionPeriodController.get('/exemptionperiod', function (req, res) {
-    req.session.data['ExemptionPeriod_1'] = req.session.data['ExemptionPeriod']
+/*router.get('/session', function (req, res) {
+  res.json(req.session)
+});*/
+
+router.post('/save-company', function (req, res) {
+  const {company} = req.session.data;
+  req.session.data = {
+    company
+  };
+
+  req.session.save(()=>{
+    res.redirect('/Dashboard');
+  })
+});
+
+router.post('/UserServices', function (req, res) {
+  const { company } = req.session.data;
+  req.session.data = { company };
+  req.session.save(() => {
+    res.redirect('/UserServices');
+  })
+})
+
+router.post('/Userjourney', function (req, res) {
+  const { Userservice } = req.session.data;
+  req.session.data['complete'] = 'No';
+  req.session.data['amendcomplete'] = 'No';
+
+  req.session.save(() => {
+    if (Userservice == "Exemption") {
+      return res.redirect("/AnnunalTurnover")
+    }
+
+    res.redirect("/amendCommodities")
+  });
+});
+
+router.post('/AnnualIncome', function (req, res) {
+  const { AnnualIncome } = req.session.data;
+
+  if (AnnualIncome < 500) {
+    return res.redirect('/ParentCompany');
+  }
+
+  return res.redirect('/PeriodOfExemption');
+});
+
+router.post('/ParentCompany', function (req, res) {
+  const { ParentCompany } = req.session.data;
+
+  if (ParentCompany === "Yes") {
+    return res.redirect('/ParentCompanyDetails')
+  }
+  res.redirect('/NoService')
+});
+
+router.post('/ParentCompanyAnnualIncome', function (req, res) {
+  const { ParentCompanyAnnualIncome } = req.session.data;
+
+  if (ParentCompanyAnnualIncome < 100) {
+    return res.redirect('/NoService');
+  }
+
+  res.redirect('/PeriodOfExemption')
+});
+
+router.post('/exemptionperiod', function (req, res) {
+  if (req.session.data.ExemptionPeriod) {
+    const periods = req.session.data.ExemptionPeriod.split(' ');
+    req.session.data['reportingPeriod'] = `${periods[1]} to ${periods[4]}`;
+  }
+
+  req.session.save(() => {
     res.redirect("/FRCcommodities");
+  });
+});
+
+router.get('/CommodityVolumeController', function (req, res) {
+  req.session.data['selectedCommodities'] = req.session.data['FRC'].map(commodity => {
+    const config = req.session.data['commodities'].find(_config => _config.value === commodity)
+    return {
+      id: config.value,
+      text: config.text,
+      value: config.value,
+      errorMessage: undefined,
+    }
+  });
+
+  req.session.save(() => {
+    res.redirect('/CommodityVolume');
+  });
+});
+
+
+router.post('/commodityvolume', function (req, res) {
+  const selectedCommodities = [...req.session.data['selectedCommodities']];
+  const error = req.session.data['errors'].commodities
+
+  Object.entries(req.body).forEach(([commodity, value]) => {
+    const index = selectedCommodities.findIndex(_config => _config.id === commodity);
+    selectedCommodities[index].commodityValue = value
+    selectedCommodities[index].errorMessage = value > 999 ? error.invalid : undefined
+  });
+  req.session.data['selectedCommodities'] = selectedCommodities
+
+  req.session.save(() => {
+    if (selectedCommodities.find(commodity => commodity.errorMessage != undefined)) {
+      return res.redirect("/CommodityVolume")
+    }
+
+    res.redirect("/CommodityDetermination")
+  });
+});
+
+
+router.post('/getdata_amended_threshold', function (req, res) {
+  req.session.data['amendcomplete'] = 'Yes';
+
+  const fromYear = req.session.data['threshold-change-year'];
+  const fromMonth = req.session.data['threshold-change-month'];
+  const fromDay = req.session.data['threshold-change-day'];
+  const fromPeriod = new Date(`${fromYear}-${fromMonth}-${fromDay}`).toLocaleString('en-us', {
+    month: 'short', year: 'numeric'
+  });
+  const toPeriod = req.session.data['ExemptionPeriod'].split("to")[1];
+
+  req.session.data['thresholdchange_Amend'] = `${fromPeriod} to ${toPeriod}`;
+  req.session.data['amendedCommodities'] = req.session.data['amendedCommodity'].map(commodity => {
+    const config = req.session.data['commodities'].find(_config => _config.value === commodity)
+    return {
+      id: config.value,
+      text: config.text,
+    }
+  });
+
+  req.session.save(() => {
+    res.redirect("/amendCommodityMethods");
+  });
+});
+
+router.post('/appcomplete', function (req, res) {
+
+  const today = new Date();
+  const yyyy = today.getFullYear();
+  let mm = today.getMonth() + 1; // Months start at 0!
+  let dd = today.getDate();
+
+  if (dd < 10) dd = '0' + dd;
+  if (mm < 10) mm = '0' + mm;
+
+  const currentDate = dd + '/' + mm + '/' + yyyy;
+  req.session.data['currentdate'] = currentDate;
+
+  req.session.data['complete'] = 'Yes';
+  res.redirect("/applicationcomplete");
+
+});
+
+/*
+Static_AmendedCommodityController.post('/getdata_amend', function (req, res) {
+  if (req.session.data['static_amend_commodity'] == "Soy") {
+    res.redirect("/thresholdchange_soya");
+  }
+  else {
+    res.redirect("/thresholdchange_rubber");
+  }
+
+});
+
+AppComplete_AmendedCommodityController.post('/getdata_appcomplete', function (req, res) {
+  if (req.session.data['static_amend_commodity'] == "Soy") {
+    res.redirect("/application_complete_Soy");
+  }
+  else {
+    res.redirect("/application_complete_rubber");
+  }
 
-  },
-
-    CommodityMethodsController.get('/CommodityMethodsController', function (req, res) {
-      res.redirect("/CommodityFileUpload");
-
-    },
-      CommodityVolumeGetController_Router.get('/CommodityVolumeGetController', function (req, res) {
-
-
-
-        req.session.data['selectedCommodities'] = req.session.data['FRC'].map(commodity => {
-
-          const config = req.session.data['commodities'].find(_config => _config.value === commodity)
-
-          return {
-
-            id: config.value,
-
-            text: config.text,
-
-            errorMessage: undefined
-
-          }
-
-        })
-        res.redirect('/CommodityVolumePage')
-
-
-      },
-
-
-        AnnualIncomeController.post('/AnnualIncome', function (req, res) {
-
-
-          var AnnualIncome = req.session.data['AnnualIncome'];
-          if (AnnualIncome < 500) {
-            res.redirect('/ParentCompany');
-          }
-          else if (AnnualIncome >= 500) {
-            res.redirect('/PeriodOfExemption');
-          }
-        },
-          ParentCompanyController.post('/ParentCompany', function (req, res) {
-
-
-            var ParentCompany = req.session.data['ParentCompany'];
-            var AnnualIncome = req.session.data['AnnualIncome'];
-
-
-            if (ParentCompany == "No") {
-
-
-              if (AnnualIncome < 500) { res.redirect('/NoService') }
-
-
-            } else if (ParentCompany == "Yes") {
-
-              res.redirect('/ParentCompanyDetails')
-            }
-
-          },
-            ParentCompanyIncomeController.post('/ParentCompanyAnnualIncome', function (req, res) {
-
-
-              var ParentCompanyAnnualIncome = req.session.data['ParentCompanyAnnualIncome'];
-
-              if (ParentCompanyAnnualIncome < 100) {
-                res.redirect('/NoService')
-              }
-
-
-              else {
-
-                res.redirect('/PeriodOfExemption')
-              }
-
-            },
-              UserjourneyController.post('/Userjourney', function (req, res) {
-                const { Userservice } = req.session.data;
-                req.session.data = { Userservice };
-
-                req.session.data['complete'] = 'No';
-                req.session.data['amendcomplete'] = 'No';
-
-
-
-
-                if (Userservice == "Exemption") {
-                  res.redirect("/AnnunalTurnover")
-
-                }
-                else {
-                  res.redirect("/amendCommodities")
-                }
-
-
-
-              },
-
-                commodityvolumeController.post('/commodityvolume', function (req, res) {
-
-                  let count = 0;
-                  let FRC = new Array();
-                  FRC = req.session.data['FRC'];
-                  let selcommodities = new Array();
-
-
-                  var commodityvolume = req.session.data['commodityvolume'];
-                  var AnnualIncome1 = req.session.data['AnnualIncome'];
-                  var Descritption = req.session.data['description'];
-                  let fromYear = req.session.data['fromYear'];
-                  let toYear = req.session.data['toYear'];
-                  let count1 = 0;
-
-
-                  const selectedCommodities = [...req.session.data['selectedCommodities']];
-
-                  const error = req.session.data['errors'].commodities
-
-                  Object.entries(req.body).forEach(([commodity, value]) => {
-
-                    const index = selectedCommodities.findIndex(_config => _config.id === commodity);
-                    selectedCommodities[index].value = value
-                    selectedCommodities[index].errorMessage = value > 999 ? error.invalid : undefined
-                    selcommodities[index] = commodity
-                  });
-                  req.session.data['selectedCommodities'] = selectedCommodities
-                  req.session.data['selcommodities'] = selcommodities
-                  const sc = [...req.session.data['selcommodities']];
-
-                  for (let i = 0; i < req.session.data['selectedCommodities'].length; i++) {
-                    if (selectedCommodities[i].errorMessage != undefined) {
-                      count1++;
-                    }
-
-                  }
-                  if (count1 > 0) {
-                    return res.redirect("/CommodityVolumePage")
-                  }
-                  else {
-                    res.redirect("/CommodityDetermination")
-                  }
-                },
-                  applicationcompletionController.post('/appcomplete', function (req, res) {
-
-                    const today = new Date();
-                    const yyyy = today.getFullYear();
-                    let mm = today.getMonth() + 1; // Months start at 0!
-                    let dd = today.getDate();
-
-                    if (dd < 10) dd = '0' + dd;
-                    if (mm < 10) mm = '0' + mm;
-
-                    const currentDate = dd + '/' + mm + '/' + yyyy;
-                    req.session.data['currentdate'] = currentDate;
-
-                    req.session.data['complete'] = 'Yes';
-                    res.redirect("/applicationcomplete");
-
-                  },
-
-                    AmendedCommodityController.post('/getdata', function (req, res) {
-
-
-                      for (let i = 0; i < req.session.data['selectedCommodities'].length; i++) {
-                        if (req.session.data['selectedCommodities'][i].value == req.session.data['ac']) {
-
-                          amendedcommodity = req.session.data['selectedCommodities'][i].text
-                        }
-
-                      }
-
-                      req.session.data['amendedcommodity'] = amendedcommodity;
-
-                      res.redirect("/thresholdchange");
-
-                    },
-                      AmendedExemptionPeriodController.post('/getdata_amended_threshold', function (req, res) {
-                        let amendedcommodity = "";
-                        req.session.data['amendcomplete'] = 'Yes';
-
-                        let fromMonth_amend = req.session.data['threshold-change-month'];
-                        let fromYear_amend = req.session.data['threshold-change-year'];
-                        let period = req.session.data['ExemptionPeriod_1'];
-                        const myArray = period.split("-");
-                        let toperiod = myArray[1];
-
-
-
-                        function getMonthName1(monthNumber) {
-                          const date = new Date();
-                          date.setMonth(monthNumber - 1);
-
-                          return date.toLocaleString('en-US', {
-                            month: 'long',
-                          });
-                        }
-                        req.session.data['FromPeriod_amend'] = getMonthName1(fromMonth_amend).concat(" ", fromYear_amend);
-                        req.session.data['thresholdchange_Amend'] = getMonthName1(fromMonth_amend).concat(" ", fromYear_amend).concat("-", toperiod);
-                        res.redirect("/amendCommodityMethods");
-                      },
-
-                        Static_AmendedCommodityController.post('/getdata_amend', function (req, res) {
-                          if (req.session.data['static_amend_commodity'] == "Soy") {
-                            res.redirect("/thresholdchange_soya");
-                          }
-                          else {
-                            res.redirect("/thresholdchange_rubber");
-                          }
-
-                        },
-
-                          AppComplete_AmendedCommodityController.post('/getdata_appcomplete', function (req, res) {
-                            if (req.session.data['static_amend_commodity'] == "Soy") {
-                              res.redirect("/application_complete_Soy");
-                            }
-                            else {
-                              res.redirect("/application_complete_rubber");
-                            }
-
-                          },
-                          )
-                        )
-
-                      )
-
-
-                    )
-                  )
-                )
-              )
-            )
-          )
-        )
-      )
-    )
-
-  )
-)
+});*/
